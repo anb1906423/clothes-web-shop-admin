4237c3047e77776278825e09dcb9215b7960a555
anb1906423 <100342430+anb1906423@users.noreply.github.com>
2023-03-16 13:08:55 +0700

    16/03

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { FaUser, FaSignOutAlt } from 'react-icons/fa'
+import { LogoutOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Divider, Radio, Space } from 'antd';
 
 const Header = (props) => {
+  const dispatch = useDispatch()
+  const userInfo = useSelector((state) => state.user.userInfo)
+
+  useEffect(() => {
+    console.log(userInfo);
+  })
+
   return (
-    <div className="header d-flex align-items-center">
-        <h6 className="title-header">{props.title}</h6>
+    <div className="header d-flex align-items-center justify-content-between">
+      <h6 className="title-header">{props.title}</h6>
+      <div className="account-box d-flex justify-content-between">
+        <div className="user-icon-box position-relative">
+          <Button
+            className="d-flex align-items-center justify-content-center"
+            type=""
+            icon={<UserOutlined />}
+            ghost='true'
+            danger='true'
+            style={{
+              border: "1.5px solid #000",
+            }}
+          >
+            {userInfo.email}
+          </Button>
+        </div>
+        <div className="logout-box">
+          <Button
+            className="btn btn-dark d-flex align-items-center justify-content-center"
+            type=""
+            icon={<LogoutOutlined />}
+            danger='true'
+            style={{ backgroundColor: "#000" }}
+          >
+            Đăng xuất
+          </Button>
+        </div>
+      </div>
     </div>
   )
 }
